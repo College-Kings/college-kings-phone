e78ce9f6434d78f0526c966df0aa9a0b8b86cb14
KiloOscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-03 07:45:52 +0100

    fixed typing

--- MessengerBuilder_ren.py
+++ MessengerBuilder_ren.py
@@ -1,11 +1,11 @@
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, Self
 
 from renpy import store
 from renpy.exports import SetVariable
 
 from game.characters.NonPlayableCharacter_ren import NonPlayableCharacter
 from game.phone.Message_ren import Message
-from game.phone.MessengerService_ren import MessengerService
+from game.phone.messenger.MessengerService_ren import MessengerService
 from game.phone.Reply_ren import Reply
 
 
@@ -20,12 +20,12 @@ class MessageBuilder:
         self.clear_pending: bool = clear_pending
         self.message_queue: list[Message] = []
         self.current_message: Optional[Message] = None
-        self.functions: list[Callable] = []
+        self.functions: list[tuple[Callable, tuple, dict]] = []
 
     def __repr__(self) -> str:
         return f"MessageBuilder({self.contact})"
 
-    def new_message(self, content: str, *replies: Reply) -> None:
+    def new_message(self, content: str, *replies: Reply) -> Self:
         self.current_message = Message(self.contact, content, replies)
         self.message_queue.append(self.current_message)
 
@@ -33,12 +33,12 @@ class MessageBuilder:
 
         return self
 
-    def add_reply(self, content: str, next_message=None) -> None:
+    def add_reply(self, content: str, next_message=None) -> Self:
         self.add_replies(Reply(content, next_message))
 
         return self
 
-    def add_replies(self, *replies: Reply) -> None:
+    def add_replies(self, *replies: Reply) -> Self:
         if self.current_message is None or self.current_message.replies:
             return self.new_message("", *replies)
 
@@ -46,12 +46,12 @@ class MessageBuilder:
 
         return self
 
-    def add_function(self, function: Callable, *args, **kwargs) -> None:
+    def add_function(self, function: Callable, *args, **kwargs) -> Self:
         self.functions.append((function, args, kwargs))
 
         return self
 
-    def set_variable(self, var_name: str, value: Any) -> None:
+    def set_variable(self, var_name: str, value: Any) -> Self:
         self.add_function(SetVariable(var_name, value))
 
         return self
