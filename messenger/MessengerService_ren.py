9de0e7c684636e326fe9d918140277bb481914ee
KiloOscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-09 03:24:30 +0100

    fix add replies method

--- messenger/MessengerService_ren.py
+++ messenger/MessengerService_ren.py
@@ -54,7 +54,7 @@ class MessengerService:
             not contact.pending_text_messages
             or contact.pending_text_messages[0].replies
         ):
-            MessengerService.new_message(contact, "", *replies)
+            return MessengerService.new_message(contact, "", *replies)
 
         contact.pending_text_messages[-1].replies = replies
 
