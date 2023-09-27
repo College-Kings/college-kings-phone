6fe0bf43bf191c7c1caf4d4b48579d4b9c4bb038
KiloOscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2023-09-27 01:43:34 +0100

    fixed typing

--- TrackerData_ren.py
+++ TrackerData_ren.py
@@ -1,20 +1,19 @@
+from __future__ import annotations
+from typing import ClassVar
+from dataclasses import dataclass
+
+from game.characters.NonPlayableCharacter_ren import NonPlayableCharacter
+
 """renpy
 init python:
 """
-
-from __future__ import annotations
-from typing import TYPE_CHECKING, ClassVar
-from dataclasses import dataclass
-
-if TYPE_CHECKING:
-    from game.characters.NonPlayableCharacter_ren import NonPlayableCharacter
 
 
 @dataclass
 class TrackerData:
     data: ClassVar[list[TrackerData]] = []
 
-    character: NonPlayableCharacter
+    character: "NonPlayableCharacter"
     condition: bool
     true_text: str = ""
     false_text: str = ""
