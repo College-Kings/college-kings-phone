00c9ce3d34e58b61e8d7069674a256a3e953200d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-03-20 00:55:02 +0000

    Add __setstate__ method to Kiwii class

--- kiwii/kiwii_ren.py
+++ kiwii/kiwii_ren.py
@@ -1,3 +1,4 @@
+from typing import Any
 from game.phone.Application_ren import Application
 from game.phone.kiwii.KiwiiPost_ren import KiwiiPost
 from game.phone.kiwii.KiwiiService_ren import KiwiiService
@@ -10,6 +11,16 @@ init python:
 class Kiwii(Application, object):
     def __init__(self) -> None:
         self.posts: list[KiwiiPost] = []
+
+    def __setstate__(self, state: dict[str, Any]) -> None:
+        self.__init__()
+
+        state.setdefault("posts", [])
+        state["posts"] += state.pop("_posts", [])
+
+        state.pop("contacts", None)
+
+        self.__dict__.update(state)
 
     @property
     def notification(self) -> bool:
