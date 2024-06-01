4874c75210d2f07123faf752045c5e3dc9207672
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-06-01 23:12:54 +0100

    Refactored Message __repr__ method and added `from_reply` classmethod

--- Message_ren.py
+++ Message_ren.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
 
 from game.characters.NonPlayableCharacter_ren import NonPlayableCharacter
+from game.characters.character_ren import Character
+from game.characters.main_character_ren import MainCharacter
 from game.phone.messenger.Reply_ren import Reply
+
+mc = MainCharacter()
 
 """renpy
 init python:
@@ -10,12 +14,12 @@ init python:
 
 @dataclass
 class Message:
-    contact: NonPlayableCharacter
+    contact: Character
     content: str
     replies: tuple["Reply", ...] = ()
 
     def __repr__(self) -> str:
-        return f"Message({self.__dict__})"
+        return f"{self.__class__.__name__}({self.contact.name}, {self.content=}, {self.replies})"
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, Message):
@@ -27,5 +31,14 @@ class Message:
             and self.replies == __value.replies
         )
 
+    @classmethod
+    def from_reply(cls, reply: "Reply") -> "Message":
+        return cls(mc, reply.content, ())
+
     def send(self) -> None:
+        if not isinstance(self.contact, NonPlayableCharacter):
+            raise TypeError(
+                f"Only NonPlayableCharacters can receive messages, not {self.contact}"
+            )
+
         self.contact.text_messages.append(self)
